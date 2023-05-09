6efb16be7ce71f3d27f2cb393f96bbafdccb9bfc
Alex Tkachev <alex.tkachev@paymentology.com>
2023-05-09 12:02:37 +0300

    main page Benefits section

--- src/pages/Home/Benefits/Benefits.js
+++ src/pages/Home/Benefits/Benefits.js
@@ -5,38 +5,54 @@ import { Icons } from '../../../components/organism/Icons/Icons'
 
 const Benefits = () => {
   return (
-    <section className={clsx(styles.benefits, 'inner')}>
-      <ul className={styles.benefitsList}>
-        <li className={styles.benefitsItem}>
-          <Icons name="icon-benefits-1" className={styles.benefitsItemIcon} />
-          <h8 className={styles.benefitsItemHeading}>
-            Унікальний рецепт тіста
-          </h8>
-          <p className={styles.benefitsItemDescription}>
-            Вершкова основа, моцарелла, горгондзола, груша, копчене куряче філе,
-            соус бальзамік
-          </p>
-        </li>
-        <li className={styles.benefitsItem}>
-          <Icons name="icon-benefits-2" className={styles.benefitsItemIcon} />
-          <h8 className={styles.benefitsItemHeading}>Найсвіжіші продукти</h8>
-          <p className={styles.benefitsItemDescription}>
-            Вершкова основа, моцарелла, горгондзола, груша, копчене куряче філе,
-            соус бальзамік
-          </p>
-        </li>
-        <li className={styles.benefitsItem}>
-          <Icons name="icon-benefits-3" className={styles.benefitsItemIcon} />
-          <h8 className={styles.benefitsItemHeading}>Випікаємо на дровах</h8>
-          <p className={styles.benefitsItemDescription}>
-            Вершкова основа, моцарелла, горгондзола, груша, копчене куряче філе,
-            соус бальзамік
-          </p>
-        </li>
-        <li className={clsx(styles.benefitsItem, styles.benefitsItemPicture)}>
-          <img src={benefitsImage} alt="" />
-        </li>
-      </ul>
+    <section className={styles.benefits}>
+      <div className="inner">
+        <h5 className={styles.benefitsHeading}>
+          БAZILIO — Це справжня піца з дров'яної печі
+        </h5>
+
+        <ul className={styles.benefitsList}>
+          <li className={styles.benefitsItem}>
+            <Icons name="icon-benefits-1" className={styles.benefitsItemIcon} />
+            <div>
+              <h6 className={styles.benefitsItemHeading}>
+                Унікальний рецепт тіста
+              </h6>
+              <p className={styles.benefitsItemDescription}>
+                Вершкова основа, моцарелла, горгондзола, груша, копчене куряче
+                філе, соус бальзамік
+              </p>
+            </div>
+          </li>
+          <li className={styles.benefitsItem}>
+            <Icons name="icon-benefits-2" className={styles.benefitsItemIcon} />
+            <div>
+              <h6 className={styles.benefitsItemHeading}>
+                Найсвіжіші продукти
+              </h6>
+              <p className={styles.benefitsItemDescription}>
+                Вершкова основа, моцарелла, горгондзола, груша, копчене куряче
+                філе, соус бальзамік
+              </p>
+            </div>
+          </li>
+          <li className={styles.benefitsItem}>
+            <Icons name="icon-benefits-3" className={styles.benefitsItemIcon} />
+            <div>
+              <h6 className={styles.benefitsItemHeading}>
+                Випікаємо на дровах
+              </h6>
+              <p className={styles.benefitsItemDescription}>
+                Вершкова основа, моцарелла, горгондзола, груша, копчене куряче
+                філе, соус бальзамік
+              </p>
+            </div>
+          </li>
+          <li className={clsx(styles.benefitsItem, styles.benefitsItemPicture)}>
+            <img src={benefitsImage} alt="" />
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
